b827af82cd5d44d135ddaef6d65c88fcd5e5e8fc
palakSingh621 <kushwahps621@gmail.com>
2025-03-24 15:41:46 +0530

    [Palak] Add: UC2 Completing the frontend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  showLoginForm: boolean = true;
 
+  toggleForm() {
+    this.showLoginForm = !this.showLoginForm;
+  }
   constructor() { }
 
   ngOnInit(): void {
